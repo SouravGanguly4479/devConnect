3172c034b7e42a103dfc2c8e234ff30ac05bc556
SouravGanguly4479 <souravganguly_s@srmuniv.edu.in>
2019-04-03 04:22:26 +0530

    3rd April 2019 || 04:22:21

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,6 +1,17 @@
 //Contain details regarding the users 
 const express =require('express');
 const router = express.Router();
+const mongoose = require('mongoose');
+const passport = require('passport');
+
+
+//Load Profile Model
+const Profile = require('../../models/Profile');
+//Load User Model
+const User = require('../../models/User');
+
+
+
 
 // @route GET api/profile/test
 // @desc  test profile routes
@@ -8,5 +19,25 @@ const router = express.Router();
 
 router.get('/profile', (req,res) => res.json({msg :'It Works....'}));
 
+// @route GET api/profile
+// @desc  GEt current user profile
+// @acess Private
+
+router.get('/',passport.authenticate('jwt',{session:false}),(req,res) =>{
+    /* res.json({msg: 'Hello'}); */
+
+    const errors = {};
+    Profile.findOne( {user:req.user.id})
+            .then(profile =>{
+                if(!profile){
+
+                    errors.noprofile = 'There is no profile for this user';
+                    res.status(404).json(errors);
+                }else{
+                res.json(profile);
+                }
+            }).catch(err => res.status(404).json(err));
+
+});
 
 module.exports = router;
